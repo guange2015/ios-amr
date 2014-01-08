86c9ddec09d2e90951ea496121c5cdb529292330
hhuai <8863824@gmail.com>
2014-01-08 14:22:27 +0800

    增加i386 arm64支持

--- openamr/Readme.md
+++ openamr/Readme.md
@@ -3,3 +3,7 @@
 编译方法
 
 sh ./build-iphone.sh
+
+详细参考请转
+
+https://github.com/feuvan/opencore-amr-iOS
--- openamr/build-iphone.sh
+++ openamr/build-iphone.sh
@@ -1,42 +1,61 @@
 #!/bin/sh
-
+### Modified from http://blog.csdn.net/favormm/article/details/6772097
 set -xe
 
-PATH=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin:$PATH
-SDK=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS6.1.sdk
-DEST=/tmp/opencore-amr-iphone
-
-ARCHS="armv7 armv7s"
+DEVELOPER=`xcode-select -print-path`
+DEST=`pwd .`"/opencore-amr-iOS"
+ 
+ARCHS="i386 x86_64 armv7 armv7s arm64"
 LIBS="libopencore-amrnb.a libopencore-amrwb.a"
+# Note that AMR-NB is for narrow band http://en.wikipedia.org/wiki/Adaptive_Multi-Rate_audio_codec
+# for AMR-WB encoding, refer to http://sourceforge.net/projects/opencore-amr/files/vo-amrwbenc/
+# or AMR Codecs as Shared Libraries http://www.penguin.cz/~utx/amr
+
+mkdir -p $DEST
+
+./configure
 
 for arch in $ARCHS; do
-	case $arch in
-	arm*)
-		CC="gcc -arch $arch --sysroot=$SDK" CXX="g++ -arch $arch --sysroot=$SDK" \
-		LDFLAGS="-Wl,-syslibroot,$SDK" ./configure \
-		--host=arm-apple-darwin --prefix=$DEST \
-		--disable-shared --enable-gcc-armv5
-		;;
-	*)
-		CC="gcc -arch $arch" CXX="g++ -arch $arch" \
-		./configure \
-		--prefix=$DEST \
-		--disable-shared
-		;;
-	esac
-	make -j3
-	make install
-	make clean
-	for i in $LIBS; do
-		mv $DEST/lib/$i $DEST/lib/$i.$arch
-	done
+        make clean
+        IOSMV=" -miphoneos-version-min=5.0"
+        case $arch in
+        arm*)
+                if [ $arch == "arm64" ]
+                then
+                        IOSMV=" -miphoneos-version-min=7.0"
+                fi
+                echo "Building opencore-amr for iPhoneOS $arch ****************"
+                PATH=`xcodebuild -version -sdk iphoneos PlatformPath`"/Developer/usr/bin:$PATH" \
+                SDK=`xcodebuild -version -sdk iphoneos Path` \
+                CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include" \
+                LDFLAGS="-Wl,-syslibroot,$SDK" \
+                ./configure \
+                --host=arm-apple-darwin \
+                --prefix=$DEST \
+                --disable-shared
+                ;;
+        *)
+                echo "Building opencore-amr for iPhoneSimulator $arch *****************"
+                PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
+                CXX="xcrun --sdk iphonesimulator clang++ -arch $arch $IOSMV -miphoneos-version-min=5.0" \
+                ./configure \
+                --prefix=$DEST \
+                --disable-shared
+                ;;
+        esac
+        make -j3
+        make install
+        for i in $LIBS; do
+                mv $DEST/lib/$i $DEST/lib/$i.$arch
+        done
 done
+
+echo "Merge into universal binary."
 
 for i in $LIBS; do
-	input=""
-	for arch in $ARCHS; do
-		input="$input $DEST/lib/$i.$arch"
-	done
-	lipo -create -output $DEST/lib/$i $input
+        input=""
+        for arch in $ARCHS; do
+                input="$input $DEST/lib/$i.$arch"
+        done
+        xcrun lipo -create -output $DEST/lib/$i $input
 done
-
